d03d8d43997fea8efd08f956f4b90e5472fde08e
nikhilrajbhar <nikhilrajbhar@users.noreply.github.com>
2021-02-01 20:20:46 +0530

    chane in connnectiom

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try{
         // mongodb connection string
-        const con = await mongoose.connect(process.env.MONGO_URI, {
+        const con = await mongoose.connect('mongodb+srv://admin:admin@cluster0.ozcp8.mongodb.net/users?retryWrites=true&w=majority', {
             useNewUrlParser: true,
+            
             useUnifiedTopology: true,
             useFindAndModify: false,
             useCreateIndex: true
